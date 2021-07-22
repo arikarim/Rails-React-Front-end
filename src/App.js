447bab9583bda0db182ecdbe492548848d495dc8
arikarim <arikarim146@gmail.com>
2021-07-22 21:31:11 +0300

    add componenets

--- src/App.js
+++ src/App.js
@@ -5,48 +5,33 @@ import { useEffect, useState } from 'react';
 
 
 function App() {
-  const [input, setInput] = useState('')
+  const [token, setToken] = useState('')
 
   const fetchData = async() => {
     const heroku = 'https://afternoon-coast-71095.herokuapp.com'
-    const data = await axios.post('http://localhost:3001/users/sign_in', 
+    const data = await axios.post('https://afternoon-coast-71095.herokuapp.com/users/sign_in', 
     {
       "user": {
         "email": "ttestt@example.com",
         "password": "12345678"
       }
-    },
-  
-  {withCredentials: true}
+    }
       );
-      const headers = {
+      console.log(data);
+      if (data.headers.authorization) {
+        const dataa = await axios.get('https://afternoon-coast-71095.herokuapp.com/member',{
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': data.headers.authorization
+          }
+      })
+        console.log(dataa.data.message);
 
       }
-      console.log(data);
-      const dataa = await axios.get('http://localhost:3001/member',{
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': data.headers.authorization
-        }
-      },{withCredentials: true});
-      console.log(dataa.data.message);
   }
-  // const fetchDataa = async() => {
-  //   const headers = {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${input}`
-  //   }
-  //   console.log(headers.Authorization);
-  //   const dataa = await axios.get('https://fierce-tundra-36709.herokuapp.com/member-data', {headers});
-  //   console.log(data);
-  //   return data;
-  // }
 
   useEffect(() => {
     fetchData();
-    // setTimeout(() => {
-    //   fetchDataa();
-    // }, 2000);
   }, [])
   return (
     <div className="App">
---
+++ src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <form>
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="password" />
+    </form>
+  )
+}
+
+export default Signup
